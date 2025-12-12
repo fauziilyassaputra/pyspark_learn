eabbc75783be968f3951f2b82bcce25925d1296d
fauzi ilyas saputra <fauziilyassaputra@gmail.com>
2025-12-12 07:03:32 +0700

    Update learn_pyspark.py

--- learn_pyspark.py
+++ learn_pyspark.py
@@ -9,7 +9,18 @@ from pyspark.sql import SparkSession
 # gerbang utama
 spark= SparkSession.builder.appName('practices').getOrCreate()
 
+# read
 df_spark = spark.read.csv('data.csv')
+
+# show data
 # df_spark.show()
+
+# read with header
 # df_spark=spark.read.option('header', 'true').csv('data.csv').show()
+
+# tipe variable didalamnya
 print(type(df_spark))
+
+# read schema
+df_spark.printSchema()
+
